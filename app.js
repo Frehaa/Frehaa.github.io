1509fb0fd8e56b1ace3b788e7e85348a8bb29001
Frederik Haagensen <haag@itu.dk>
2022-10-23 21:11:45 +0200

    Wu with alpha blending

--- app.js
+++ app.js
@@ -4,10 +4,31 @@ let lastTimestamp = 0;
 let mouseX = 0;
 let mouseY = 0;
 
-function clamp(x, minimum, maximum) {
-    if (x > maximum) x = maximum;
-    else if (x < minimum) x = minimum;
-    return x;
+function animateWuLine(currentTimestamp) {
+    let canvas = document.getElementById("wuline");
+    let width = canvas.width;
+    let height = canvas.height;
+    let ctx = canvas.getContext("2d");
+    let imageData = ctx.getImageData(0, 0, width, height);
+
+    clear(imageData, rgb(0, 0, 1));
+
+    // Scaled 
+    // let x = Math.floor(clamp(mouseX - canvas.offsetLeft, 0, canvas.width - 1) / 20);
+    // let y = Math.floor(clamp(mouseY - canvas.offsetTop, 0, canvas.height - 1) / 20);
+    // drawWuLine(
+    //     imageData, 
+    //     width / 40, height / 40, 
+    //     x, y, 
+    //     rgb(1, 0, 0), 
+    //     (imageData, x, y, c) => writeColorSim(imageData, x, y, 20, c, blendColor)
+    // );
+
+    let x = clamp(mouseX - canvas.offsetLeft, 0, canvas.width - 1);
+    let y = clamp(mouseY - canvas.offsetTop, 0, canvas.height - 1);
+    drawWuLine(imageData, width/2, height/2, x, y, rgb(1, 0, 0), blendColor);
+
+    ctx.putImageData(imageData, 0, 0);
 }
 
 function animateBrezenhamLine(currentTimestamp) {
@@ -19,18 +40,22 @@ function animateBrezenhamLine(currentTimestamp) {
     let ctx = canvas.getContext("2d");
     let imageData = ctx.getImageData(0, 0, width, height);
 
-    clear(imageData, rgb(255,255, 255));
+    clear(imageData, rgb(1, 1, 1));
 
-    // Calculate changing color based on timestamp
-    let sin = Math.sin(currentTimestamp / 100);
-    let val = 128 + 50 * sin;
-    let color = rgb(255, val, val);
+    // let x = Math.floor(clamp(mouseX - canvas.offsetLeft, 0, canvas.width - 1) / 20);
+    // let y = Math.floor(clamp(mouseY - canvas.offsetTop, 0, canvas.height - 1) / 20);
+    // drawBresenhamLine(
+    //     imageData, 
+    //     width / 40, height / 40, 
+    //     x, y, 
+    //     rgb(1, 0, 0), 
+    //     (imageData, x, y, c) => writeColorSim(imageData, x, y, 20, c, writeColor)
+    // );
 
-    // Mouse stuff
     let x = clamp(mouseX - canvas.offsetLeft, 0, canvas.width - 1);
     let y = clamp(mouseY - canvas.offsetTop, 0, canvas.height - 1);
-    drawBresenhamLine(imageData, canvas.width / 2, canvas.height / 2, x, y, color);
-    
+    drawBresenhamLine(imageData, width/2, height/2, x, y, rgb(1, 0, 0), writeColor);
+
     ctx.putImageData(imageData, 0, 0);
 
     // FPS 
@@ -38,45 +63,19 @@ function animateBrezenhamLine(currentTimestamp) {
     ctx.fillText(dt.toFixed(0).toString() + " ms", 5, 20);
 }
 
-function animateBrezenhamLine(currentTimestamp) {
-    let dt = currentTimestamp - lastTimestamp;
-    lastTimestamp = currentTimestamp;
-    let canvas = document.getElementById("wuline");
-    let width = canvas.width;
-    let height = canvas.height;
-    let ctx = canvas.getContext("2d");
-    let imageData = ctx.getImageData(0, 0, width, height);
-
-    clear(imageData, rgb(255,255, 255));
-
-    // Calculate changing color based on timestamp
-    let sin = Math.sin(currentTimestamp / 100);
-    let val = 128 + 50 * sin;
-    let color = rgb(255, val, val);
-
-    // Mouse stuff
-    let x = clamp(mouseX - canvas.offsetLeft, 0, canvas.width - 1);
-    let y = clamp(mouseY - canvas.offsetTop, 0, canvas.height - 1);
-    drawWuLine(imageData, canvas.width / 2, canvas.height / 2, x, y, color);
-    
-    ctx.putImageData(imageData, 0, 0);
-}
-
 function animateBrezenhamCircle(currentTimestamp) {
-    let dt = currentTimestamp - lastTimestamp;
-    lastTimestamp = currentTimestamp;
     let canvas = document.getElementById("brezenhamcircle");
     let width = canvas.width;
     let height = canvas.height;
     let ctx = canvas.getContext("2d");
     let imageData = ctx.getImageData(0, 0, width, height);
 
-    clear(imageData, rgb(255,255, 255));
+    clear(imageData, rgb(1,1, 1));
 
     // Calculate changing color based on timestamp
     let sin = Math.sin(currentTimestamp / 100);
-    let val = 128 + 50 * sin;
-    let color = rgb(255, val, val);
+    let val = 0.5 + 0.25 * sin;
+    let color = rgb(1, val, val);
 
     // Mouse stuff
     let x = clamp(mouseX - canvas.offsetLeft, 0, canvas.width - 1);
@@ -97,11 +96,27 @@ function animateBrezenhamCircle(currentTimestamp) {
 }
 
 function animate(currentTimestamp) {
-    
     animateBrezenhamLine(currentTimestamp);
-    animateBrezenhamCircle(currentTimestamp);
+    // animateBrezenhamCircle(currentTimestamp);
     animateWuLine(currentTimestamp);
     // setTimeout(() => requestAnimationFrame(animate), 10);
+
+    // let canvas = document.getElementById("scaledPixels");
+    // let width = canvas.width;
+    // let height = canvas.height;
+    // let ctx = canvas.getContext("2d");
+    // let imageData = ctx.getImageData(0, 0, width, height);
+
+    // let pixelSize = 20;
+
+    // let simWidth = width / pixelSize;
+    // let simHeight = height / pixelSize;
+
+    // writeColorSim(imageData, 1, 1, pixelSize, rgb(1,0,0), writeColor);
+
+    // ctx.putImageData(imageData, 0, 0);
+
+
     requestAnimationFrame(animate);
 }
 
--- graphics.js
+++ graphics.js
@@ -1,22 +1,67 @@
 "use strict";
 
+// http://members.chello.at/~easyfilter/Bresenham.pdf
+
+function clamp(x, minimum, maximum) {
+    if (x > maximum) x = maximum;
+    else if (x < minimum) x = minimum;
+    return x;
+}
+
 function rgba(red, green, blue, alpha) {
+    red = clamp(red, 0, 1);
+    green = clamp(green, 0, 1);
+    blue = clamp(blue, 0, 1);
+    alpha = clamp(alpha, 0, 1);
     return {red, green, blue, alpha};
 }
 
 function rgb(red, green, blue) {
-    return rgba(red, green, blue, 255);
+    return rgba(red, green, blue, 1);
 }
 
 function pixelOffset(imageData, x, y) {
     return 4 * (y * imageData.width + x);
 }
 
-function writeColor(buf8, offset, color) {
-    buf8[offset + 0] = color.red;
-    buf8[offset + 1] = color.green;
-    buf8[offset + 2] = color.blue;
-    buf8[offset + 3] = color.alpha;
+function scaleColor(scalar, color) {
+    return {
+        red: color.red, 
+        blue: color.blue, 
+        green: color.green, 
+        alpha: scalar * color.alpha
+    };
+}
+// Assuming dest has alpha 1
+function alphaBlendInto(source, buf8, offset) {
+    buf8[offset + 0] = ((1-source.alpha) * buf8[offset + 0] + source.alpha * source.red) * 255;
+    buf8[offset + 1] = ((1-source.alpha) * buf8[offset + 1] + source.alpha * source.green) * 255;
+    buf8[offset + 2] = ((1-source.alpha) * buf8[offset + 2] + source.alpha * source.blue) * 255;
+    buf8[offset + 3] = 255;
+}
+
+function writeColor(imageData, x, y, color) {
+    let offset = pixelOffset(imageData, x, y);
+    let buf8 = imageData.data;
+    buf8[offset + 0] = color.red * 255;
+    buf8[offset + 1] = color.green * 255;
+    buf8[offset + 2] = color.blue * 255;
+    buf8[offset + 3] = color.alpha * 255;
+}
+
+function blendColor(imageData, x, y, color) {
+    let offset = pixelOffset(imageData, x, y);
+    let buf8 = imageData.data;
+    alphaBlendInto(color, buf8, offset);
+}
+
+function writeColorSim(imageData, x, y, pixelSize, color, writeColor) {
+    // console.log(color.red, color.green, color.blue, color.alpha)
+    for (let xp = x * pixelSize; xp < (x+1) * pixelSize; ++xp) {
+        for (let yp = y * pixelSize; yp < (y+1) * pixelSize; ++yp) {
+            writeColor(imageData, xp, yp, color);
+        }
+    }
 }
 
 function clear(imageData, color) {
@@ -26,23 +71,20 @@ function clear(imageData, color) {
 function fillRect(imageData, x0, y0, x1, y1, color) {
     for (var y = y0; y < y1; ++y) {
         for (var x = x0; x < x1; ++x) {
-            let offset = pixelOffset(imageData, x, y);
-            writeColor(imageData.data, offset, color);
+            writeColor(imageData, x, y, color);
         }
     }
 }
 
 function drawHorizontalLine(imageData, x, y, length, color) {
     for (var current = x; current <= x + length; ++current) {
-        let offset = pixelOffset(imageData, current, y);
-        writeColor(imageData.data, offset, color);
+        writeColor(imageData, x, y, color);
     }
 }
 
 function drawVerticalLine(imageData, x, y, length, color) {
     for (var current = y; current <= y + length; ++current) {
-        let offset = pixelOffset(imageData, x, current);
-        writeColor(imageData.data, offset, color);
+        writeColor(imageData, x, y, color);
     }
 }
 
@@ -54,7 +96,7 @@ function drawRect(imageData, x0, y0, x1, y1, color) {
 }
 
 // Bresenham's line algorithm
-function drawBresenhamLine(imageData, x0, y0, x1, y1, color) {
+function drawBresenhamLine(imageData, x0, y0, x1, y1, color, writeColor) {
     function drawBresenhamLineLow(imageData, x0, y0, x1, y1, color) {
         let dx = x1 - x0,
             dy = y1 - y0,
@@ -67,8 +109,7 @@ function drawBresenhamLine(imageData, x0, y0, x1, y1, color) {
             y = y0;
 
         for (let x = x0; x <= x1; ++x) {
-            let offset = pixelOffset(imageData, x, y);
-            writeColor(imageData.data, offset, color);
+            writeColor(imageData, x, y, color);
             if (D > 0) {
                 y = y + yi;
                 D = D + (2 * (dy - dx));
@@ -89,8 +130,7 @@ function drawBresenhamLine(imageData, x0, y0, x1, y1, color) {
             x = x0;
 
         for (let y = y0; y <= y1; ++y) {
-            let offset = pixelOffset(imageData, x, y);
-            writeColor(imageData.data, offset, color);
+            writeColor(imageData, x, y, color);
             if (D > 0) {
                 x = x + xi;
                 D = D + (2 * (dx - dy));
@@ -115,12 +155,9 @@ function drawBresenhamLine(imageData, x0, y0, x1, y1, color) {
 }
 
 // Xiaolin Wu's line algorithm
-function drawWuLine(imageData, x0, y0, x1, y1, color) {
+function drawWuLine(imageData, x0, y0, x1, y1, color, writeColor) {
     function fpart(x) {
         return x - Math.floor(x);
-    }
-    function rfpart(x) {
-        return 1 - fpart(x);
     }
     let steep = Math.abs(y1 - y0) > Math.abs(x1 - x0);
     if (steep) { // Swap
@@ -135,17 +172,63 @@ function drawWuLine(imageData, x0, y0, x1, y1, color) {
     let dx = x1 - x0;
     let dy = y1 - y0;
     let gradient = (x1 == x0)? 1.0 : dy / dx;
+    let intery = y0 + gradient * (x0 - Math.round(x0)) + gradient; 
 
-    let xend = Math.round(x0);
-    let yend = y0 + gradient * (xend - x0);
-    let xgap = fpart(x1 + 0.5);
-    let xpxl2 = xend;
-    let ypxl2 = Math.floor(yend);
-    if (steep) {
-        let offset = pixelOffset(imageData, x, y);
-        writeColor(imageData.data, offset, color);
+    // First endpoint
+    {
+        let xend = Math.round(x0);
+        let yend = y0 + gradient * (xend - x0);
+        let xgap = fpart(x1 + 0.5);
+        let xpxl1 = xend;
+        let ypxl1 = Math.floor(yend);
+        let brightness = fpart(yend) * xgap;
+        if (steep) {
+            writeColor(imageData, ypxl1, xpxl1, scaleColor(1-brightness, color));
+            writeColor(imageData, ypxl1+1, xpxl1, scaleColor(brightness, color));
+        } else {
+            writeColor(imageData, xpxl1, ypxl1, scaleColor(1-brightness, color));
+            writeColor(imageData, xpxl1, ypxl1+1, scaleColor(brightness, color));
+        }
+        intery = yend + gradient;
+    }
 
-    } else {
+    // Second endpoint
+    {
+        let xend = Math.round(x1);
+        let yend = y1 + gradient * (xend - x1);
+        let xgap = fpart(x1 + 0.5);
+        let xpxl2 = xend;
+        let ypxl2 = Math.floor(yend);
+
+        let brightness = fpart(yend) * xgap;
+        if (steep) {
+            writeColor(imageData, ypxl2, xpxl2, scaleColor(1-brightness, color));
+            writeColor(imageData, ypxl2+1, xpxl2, scaleColor(brightness, color));
+        } else {
+            writeColor(imageData, xpxl2, ypxl2, scaleColor(1-brightness, color));
+            writeColor(imageData, xpxl2, ypxl2+1, scaleColor(brightness, color));
+        }
+    }
+
+    // Main loop
+    {
+        let start = Math.round(x0);
+        let end = Math.round(x1);
+        if (steep) {
+            for (let x = start + 1; x < end; ++x) {
+                let brightness = fpart(intery);
+                writeColor(imageData, Math.floor(intery),   x, scaleColor(1-brightness, color));
+                writeColor(imageData, Math.floor(intery)+1, x, scaleColor(brightness, color));
+                intery += gradient;
+            }
+        } else {
+            for (let x = start + 1; x < end; ++x) {
+                let brightness = fpart(intery);
+                writeColor(imageData, x, Math.floor(intery), scaleColor(1-brightness, color));
+                writeColor(imageData, x, Math.floor(intery)+1, scaleColor(brightness, color));
+                intery += gradient;
+            }
+        }
 
     }
 }
