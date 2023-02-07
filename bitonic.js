8bde720c7257ca6be43c8cb12e2128175e72fd31
Frederik Haagensen <haag@itu.dk>
2023-02-07 14:15:43 +0100

    Refactor with network subscription

--- bitonic.js
+++ bitonic.js
@@ -169,239 +169,6 @@ let bitonicSlideFrame = { // Bitonic slide thing
     keyUp: function() {}
 };
 
-const LINE_DISTANCE_THRESSHOLD = 10;
-
-// TODO: Move compare and swap logic in here
-// TODO: Make compare and swap positioned in a normalized manner (0 - 1)
-class Network {
-    constructor(size) {
-        this.size = size;
-        this.values = [];
-        for (let i = 0; i < size; ++i) {
-            this.values.push(null);
-        }
-        this.compareAndSwaps = new LinkedList();
-    }
-    set(i, value) {
-        this.values[i] = value;
-    }
-    get(i) {
-        return this.values[i];
-    }
-    addCompareAndSwap(position, i, j) {
-        let value = {
-            position,
-            cas: createCompareAndSwap(i, j),
-        };
-        return this.compareAndSwaps.insertBeforePredicate(value, n => n.position > position);
-    }
-    *getCompareAndSwaps() {
-        for (let cas of this.compareAndSwaps) {
-            yield cas
-        }
-    }
-    run() {
-        let result = this.values.slice(0);
-        this.compareAndSwaps.forEach(c => c.cas(result));
-        return result;
-    }
-
-
-}
-
-// A "Drawable" is an object with a draw method
-
-// TODO: Update every draw or update once and keep state in squares?
-// Currently updates every frame
-
-// Focus is the element which has been clicked on
-// Invariant: There should be focus on no more elements than one
-
-// TODO: Implement arrow focus
-// TODO: Implement arrow removal
-// TODO: Show path of values? In different colors and dotted line?
-class NetworkFrame {
-    constructor(network, x, y, sizes, drawSettings) {
-        if (!network instanceof Network) {
-            throw new TypeError("argument has to be instance of Network");
-        }
-        this.network = network;
-        this.compareAndSwaps = new LinkedList();
-
-        this.t = {
-            borderColor: '#000000',
-            lineWidth: 3,
-            circleRadius: 8,
-            tipLength: 15,
-            tipWidth: 10,
-            ...drawSettings
-        };
-
-        // Static drawables
-        this.leftSquares = [];
-        this.rightSquares = [];
-        this.wires = [];
-
-        // Dynamic drawables
-        this.arrow = verticalArrow(0, 0, 0, this.t.tipLength, this.t.tipWidth);
-        this.arrowStartCircle = circle(0, 0, this.t.circleRadius);
-        this.arrows = {};
-
-        // Draw Flag
-        this.displayRightSquares = true;
-
-        // Hover and focus elements
-        this.focusSquareIdx = null;
-        
-        this.hoverWireIdx = null;
-        this.focusWireIdx = null;
-
-        // Setup static drawables
-        let squareLength = sizes.square;
-        let wireLength = sizes.wire;
-        let squareOffset = squareLength + sizes.offset;
-        for (let i = 0; i < this.network.size; ++i) {
-            let wireY = y + squareLength / 2 + squareOffset * i;
-            let squareY = y + squareOffset * i;
-            this.leftSquares.push(writableSquare(x, squareY, squareLength));
-            this.wires.push(horline(x + squareOffset, wireY, wireLength));
-            this.rightSquares.push(writableSquare(x + wireLength + squareOffset + sizes.offset, squareY, squareLength));
-            this.leftSquares[i].borderColor = this.t.squareBorderColor;
-            this.rightSquares[i].borderColor = this.t.squareBorderColor;
-        }
-    }
-
-    draw(ctx) {
-        ctx.lineWidth = this.t.lineWidth;
-        // Left squares and wire
-        for (let i = 0; i < this.network.size; ++i) {
-            let lSquare = this.leftSquares[i];
-            lSquare.hover = lSquare.isInside({x: mouseX, y: mouseY});
-            lSquare.focus = (i == this.focusSquareIdx);
-            lSquare.text = this.network.get(i);
-            lSquare.draw(ctx);
-
-            this.wires[i].draw(ctx);
-        }
-        
-        // Draw place arrow start circle
-        if (this.hoverWireIdx != null) {
-            let w = this.wires[this.hoverWireIdx];
-            if(this.focusWireIdx == null) {
-                this.arrowStartCircle.x = mouseX;
-                this.arrowStartCircle.y = w.y;
-            } else if (this.hoverWireIdx != this.focusWireIdx) {
-                let wireDiff = w.y - this.wires[this.focusWireIdx].y;
-                this.arrow.x = this.arrowStartCircle.x;
-                this.arrow.y = this.arrowStartCircle.y;
-                this.arrow.length = wireDiff;
-                this.arrow.draw(ctx);
-            }
-
-            this.arrowStartCircle.draw(ctx);
-        } 
-
-        // Simulate network, draw right square results, and draw arrows
-        this.updateRightSquares();
-        this.rightSquares.forEach(s => s.draw(ctx));
-        for (let k in this.arrows) {
-            this.arrows[k].forEach(d => d.draw(ctx));
-        }
-    }
-    mouseMove() {
-        this.hoverWireIdx = null;
-        for (let i = 0; i < this.network.size; ++i) {
-            let dist = this.wires[i].distance({x: mouseX, y: mouseY});
-            if (dist <= this.t.circleRadius * 2) {
-                this.hoverWireIdx = i;
-            }
-        }
-    }
-
-    mouseDown() {
-        // Handle wire focus
-        if (this.hoverWireIdx != null) {
-            this.focusWireIdx = this.hoverWireIdx;
-            this.arrowStartCircle.y = this.wires[this.hoverWireIdx].y;
-            this.focusSquareIdx = null;
-            return;
-        }
-
-        // Handle square focus
-        for (let i = 0; i < this.network.size; ++i) {
-            let s = this.leftSquares[i];
-            if (s.isInside({x: mouseX, y: mouseY})) {
-                this.focusSquareIdx = i;
-            }
-        }
-    }
-    mouseUp() {
-        // Handle place arrow mode 
-        if (this.focusWireIdx != null && this.hoverWireIdx != null && this.hoverWireIdx != this.focusWireIdx) {
-            this.arrow.x = this.arrowStartCircle.x; // Handle Edge case where the arrow x coordinate may not have been updated (?)
-
-            // Add compare-and-swap in correct order
-            let normalizedPosition = (this.arrow.x - this.wires[0].x) / this.wires[0].length;
-            let id = this.network.addCompareAndSwap(normalizedPosition, this.hoverWireIdx, this.focusWireIdx);
-            this.arrows[id] = [this.arrowStartCircle, this.arrow];
-            
-            // Create new drawables 
-            this.arrow = verticalArrow(0, 0, 0, this.t.tipLength, this.t.tipWidth);
-            this.arrowStartCircle = circle(0, 0, this.t.circleRadius);
-        }
-        this.focusWireIdx = null;
-    }
-    keyDownCallback(e) {
-        let key = e.key;
-        if (key === DELETE_KEY || key === BAKCSPACE_KEY) { // Delete value in left square
-            this.network.set(this.focusSquareIdx, null);
-            this.focusSquareIdx = null;
-        }
-        else if (this.focusSquareIdx != null && '0' <= key && key <= '9') { // Write to left square
-            this.network.set(this.focusSquareIdx, key);
-            this.focusSquareIdx = null;
-        }
-        else if (key == 'h') { // Toggle display of right squares
-            this.displayRightSquares = !this.displayRightSquares;
-        }
-    }
-    frameStart() {
-        document.addEventListener('keydown', (e) => {
-            this.keyDownCallback(e);
-        });
-    }
-    frameEnd(){
-        document.removeEventListener('keydown', (e) => {
-            this.keyDownCallback(e);
-        });
-    }
-    keyUp() {}
-    updateRightSquares() {
-        if (this.displayRightSquares) {
-            let vals = this.network.run();
-            for (let i = 0; i < vals.length; ++i) {
-                this.rightSquares[i].text = vals[i];
-            }
-        } else {
-            for (let i = 0; i < this.network.size; ++i) {
-                this.rightSquares[i].text = null;
-            }
-        }
-    }
-
-    addCompareAndSwap(position, startWire, endWire) {
-        // TODO: Handle edge case where start or end wire is out of bound
-
-        // Create new drawables 
-        let arrow = verticalArrow(0, 0, 0, this.t.tipLength, this.t.tipWidth);
-        let arrowStartCircle = circle(0, 0, this.t.circleRadius);
-
-        // Add compare-and-swap in correct order
-        let normalizedPosition = (position - this.wires[0].x) / this.wires[0].length;
-        let id = this.network.addCompareAndSwap(normalizedPosition, startWire, endWire);
-        this.arrows[id] = [arrowStartCircle, arrow];
-    }
-}
 
 // let wireFrame = {
 //     lines: [
@@ -627,27 +394,29 @@ function createCompareAndSwap(a, b) {
     }
 }
 
-function test(vals, comps) {
-    for (let c of comps) {
-        c(vals);
-    }
-}
-
 function initialize() {
     initializeEventListeners();
     // frames.push(bitonicSlideFrame);
     // frames.push(writableSquareFrame);
 
+    let canvas = document.getElementById('canvas');
+    let ctx = canvas.getContext('2d');
+    // ctx.scale(2, 2);
+
     let network = new Network(5);
-    let networkFrame = new NetworkFrame(network, 100, 100, {square: 50, wire: 400, offset: 20});
+    let networkFrame = new NetworkFrame(network, {square: 50, wire: 400, offset: 20});
 
     let exampleNetwork = new Network(16);
-    let exampleNetworkSizes = {square: 15, wire: 800, offset: 10};
-    let exampleNetworkDrawSettings = {squareBorderColor: '#FFFFFF', lineWidth: 2, circleRadius: 5, tipLength: 10, tipWidth: 7};
-    let exampleNetworkFrame = new NetworkFrame(exampleNetwork, 25, 25, exampleNetworkSizes, exampleNetworkDrawSettings);
+    let exampleNetworkDrawSettings = {squareLength: 0, wireLength: 1600, squareOffset: 35, squareBorderColor: '#FFFFFF', lineWidth: 4, circleRadius: 5, tipLength: 10, tipWidth: 7, drawBox: false};
+    let exampleNetworkFrame = new NetworkFrame(exampleNetwork, exampleNetworkDrawSettings);
+
+    for (let i = 0; i < 15; i++) {
+        exampleNetwork.addCompareAndSwap((1 / 14) * i, i, i + 1);
+        
+    }
 
     frames.push(exampleNetworkFrame);
-    frames.push(networkFrame);
+    // frames.push(networkFrame);
     frames.push(rectangleFrame);
     
     for (let i = 0; i < 6; ++i) {
---
+++ frames/network.js
@@ -0,0 +1,249 @@
+// TODO: Move compare and swap logic in here
+// TODO: Make compare and swap positioned in a normalized manner (0 - 1)
+class Network {
+    constructor(size) {
+        this.size = size;
+        this.values = [];
+        this.callbacks = [];
+        for (let i = 0; i < size; ++i) {
+            this.values.push(null);
+        }
+        this.compareAndSwaps = new LinkedList();
+    }
+    set(i, value) {
+        this.values[i] = value;
+    }
+    get(i) {
+        return this.values[i];
+    }
+    addCompareAndSwap(position, i, j) {
+        let value = {
+            position,
+            cas: createCompareAndSwap(i, j),
+        };
+        let id = this.compareAndSwaps.insertBeforePredicate(value, n => n.position > position);
+        this.callbacks.forEach(c => c(id, position, i, j));
+    }
+    *getCompareAndSwaps() {
+        for (let cas of this.compareAndSwaps) {
+            yield cas
+        }
+    }
+    run() {
+        let result = this.values.slice(0);
+        this.compareAndSwaps.forEach(c => c.cas(result));
+        return result;
+    }
+
+    subscribe(callback) {
+        this.callbacks.push(callback);
+    }
+
+    // TODO: Unsubscribe
+
+}
+
+// A "Drawable" is an object with a draw method
+
+// TODO: Update every draw or update once and keep state in squares?
+// Currently updates every frame
+
+// Focus is the element which has been clicked on
+// Invariant: There should be focus on no more elements than one
+
+// TODO: Implement arrow focus
+// TODO: Implement arrow removal
+// TODO: Show path of values? In different colors and dotted line?
+class NetworkFrame {
+    constructor(network, drawSettings) {
+        if (!network instanceof Network) {
+            throw new TypeError("argument has to be instance of Network");
+        }
+        this.drawSettings = {
+            marginX: 25, 
+            marginY: 25,
+            borderColor: '#000000',
+            lineWidth: 3,
+            circleRadius: 8,
+            tipLength: 15,
+            tipWidth: 10,
+            squareLength: 25,
+            squareOffset: 10,
+            wireLength: 400,
+            ...drawSettings // Overwrite if available
+        };
+
+        this.network = network;
+        this.compareAndSwaps = new LinkedList();
+
+        // Static drawables
+        this.leftSquares = [];
+        this.rightSquares = [];
+        this.wires = [];
+
+        // Dynamic drawables
+        this.arrow = verticalArrow(0, 0, 0, this.drawSettings.tipLength, this.drawSettings.tipWidth);
+        this.arrowStartCircle = circle(0, 0, this.drawSettings.circleRadius);
+        this.arrows = {};
+
+        // Draw Flag
+        this.displayRightSquares = true;
+
+        // Hover and focus elements
+        this.focusSquareIdx = null;
+        
+        this.hoverWireIdx = null;
+        this.focusWireIdx = null;
+
+        // Setup static drawables
+        let squareLength = this.drawSettings.squareLength;
+        let wireLength = this.drawSettings.wireLength;
+        let squareOffset = this.drawSettings.squareOffset;
+        for (let i = 0; i < this.network.size; ++i) {
+            let wireY = this.drawSettings.marginY + squareLength / 2 + (squareLength + squareOffset) * i;
+            let squareY = this.drawSettings.marginY + (squareLength + squareOffset) * i;
+            this.leftSquares.push(writableSquare(this.drawSettings.marginX, squareY, squareLength));
+            this.wires.push(horline(this.drawSettings.marginX + squareLength + squareOffset, wireY, wireLength));
+            this.rightSquares.push(writableSquare(this.drawSettings.marginX + wireLength + squareLength + 2 * squareOffset, squareY, squareLength));
+            this.leftSquares[i].borderColor = this.drawSettings.squareBorderColor;
+            this.rightSquares[i].borderColor = this.drawSettings.squareBorderColor;
+        }
+
+        network.subscribe(this.createNetworkCasCallback());
+    }
+
+    draw(ctx) {
+        ctx.lineWidth = this.drawSettings.lineWidth;
+        // Left squares and wire
+        for (let i = 0; i < this.network.size; ++i) {
+            let lSquare = this.leftSquares[i];
+            lSquare.hover = lSquare.isInside({x: mouseX, y: mouseY});
+            lSquare.focus = (i == this.focusSquareIdx);
+            lSquare.text = this.network.get(i);
+            lSquare.draw(ctx);
+
+            this.wires[i].draw(ctx);
+        }
+        
+        // Draw place arrow start circle
+        if (this.hoverWireIdx != null) {
+            let w = this.wires[this.hoverWireIdx];
+            if(this.focusWireIdx == null) {
+                this.arrowStartCircle.x = mouseX;
+                this.arrowStartCircle.y = w.y;
+            } else if (this.hoverWireIdx != this.focusWireIdx) {
+                let wireDiff = w.y - this.wires[this.focusWireIdx].y;
+                this.arrow.x = this.arrowStartCircle.x;
+                this.arrow.y = this.arrowStartCircle.y;
+                this.arrow.length = wireDiff;
+                this.arrow.draw(ctx);
+            }
+
+            this.arrowStartCircle.draw(ctx);
+        } 
+
+        // Simulate network, draw right square results, and draw arrows
+        this.updateRightSquares();
+        this.rightSquares.forEach(s => s.draw(ctx));
+        for (let k in this.arrows) {
+            this.arrows[k].forEach(d => d.draw(ctx));
+        }
+    }
+    mouseMove() {
+        this.hoverWireIdx = null;
+        for (let i = 0; i < this.network.size; ++i) {
+            let dist = this.wires[i].distance({x: mouseX, y: mouseY});
+            if (dist <= this.drawSettings.circleRadius * 2) {
+                this.hoverWireIdx = i;
+            }
+        }
+    }
+
+    mouseDown() {
+        // Handle wire focus
+        if (this.hoverWireIdx != null) {
+            this.focusWireIdx = this.hoverWireIdx;
+            this.arrowStartCircle.y = this.wires[this.hoverWireIdx].y;
+            this.focusSquareIdx = null;
+            return;
+        }
+
+        // Handle square focus
+        for (let i = 0; i < this.network.size; ++i) {
+            let s = this.leftSquares[i];
+            if (s.isInside({x: mouseX, y: mouseY})) {
+                this.focusSquareIdx = i;
+            }
+        }
+    }
+    mouseUp() {
+        // Handle place arrow mode 
+        if (this.focusWireIdx != null && this.hoverWireIdx != null && this.hoverWireIdx != this.focusWireIdx) {
+            // Add compare-and-swap in correct order
+            let normalizedPosition = (this.arrow.x - this.wires[0].x) / this.wires[0].length;
+            console.log(this.arrow.x, normalizedPosition, this.drawSettings.wireLength)
+            this.network.addCompareAndSwap(normalizedPosition, this.focusWireIdx, this.hoverWireIdx);
+            // The new arrow will be added through the callback
+        }
+        this.focusWireIdx = null;
+    }
+    keyDownCallback(e) {
+        let key = e.key;
+        if (key === DELETE_KEY || key === BAKCSPACE_KEY) { // Delete value in left square
+            this.network.set(this.focusSquareIdx, null);
+            this.focusSquareIdx = null;
+        }
+        else if (this.focusSquareIdx != null && '0' <= key && key <= '9') { // Write to left square
+            this.network.set(this.focusSquareIdx, key);
+            this.focusSquareIdx = null;
+        }
+        else if (key == 'h') { // Toggle display of right squares
+            this.displayRightSquares = !this.displayRightSquares;
+        }
+    }
+    frameStart() {
+        document.addEventListener('keydown', (e) => {
+            this.keyDownCallback(e);
+        });
+    }
+    frameEnd(){
+        document.removeEventListener('keydown', (e) => {
+            this.keyDownCallback(e);
+        });
+    }
+    keyUp() {}
+    updateRightSquares() {
+        if (this.displayRightSquares) {
+            let vals = this.network.run();
+            for (let i = 0; i < vals.length; ++i) {
+                this.rightSquares[i].text = vals[i];
+            }
+        } else {
+            for (let i = 0; i < this.network.size; ++i) {
+                this.rightSquares[i].text = null;
+            }
+        }
+    }
+
+    createNetworkCasCallback() {
+        let xOffset = this.drawSettings.marginX + this.drawSettings.squareLength + this.drawSettings.squareOffset;
+        let wires = this.wires;
+        let wireLength = this.drawSettings.wireLength;
+        let arrowTipLength = this.drawSettings.tipLength;
+        let arrowTipWidth = this.drawSettings.tipWidth;
+        let circleRadius = this.drawSettings.circleRadius;
+
+        return (id, position, startWireIdx, endWireIdx) => {
+            // Create new drawables 
+            let x = xOffset + position * wireLength;
+            let y1 = wires[startWireIdx].y;
+            let y2 = wires[endWireIdx].y;
+            console.log(xOffset, position, wireLength, x)
+            let arrow = verticalArrow(x, y1, y2 - y1, arrowTipLength, arrowTipWidth);
+            let arrowStartCircle = circle(x, y1, circleRadius);
+
+            // Add compare-and-swap in correct order
+            this.arrows[id] = [arrowStartCircle, arrow];
+        };
+    }
+}
