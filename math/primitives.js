9203c12443ae95237f9be431574d9f411eeda2ea
Frederik Haagensen <haag@itu.dk>
2023-02-10 11:02:32 +0100

    Add shuffle

--- math/primitives.js
+++ math/primitives.js
@@ -12,3 +12,22 @@ function clamp(v, min, max) {
 function lerp(v0, v1, t) {
   return v0 + t * (v1 - v0);
 }
+
+// Fisher-yates shuffle (Based on stack overflow answer)
+function shuffle(array) {
+  let currentIndex = array.length  
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    let tmp = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = tmp;
+  }
+
+  return array;
+}
