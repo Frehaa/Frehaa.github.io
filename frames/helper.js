641695631c7fb50b33c9de121d7d4992cd563f62
Frederik Haagensen <haag@itu.dk>
2023-02-22 09:30:12 +0100

    Keep array of frames frame references in combineFrames object

--- frames/helper.js
+++ frames/helper.js
@@ -1,34 +1,35 @@
 function combineFrames(...frames) {
     return {
+        frames,
         draw: function(ctx) {
-            frames.forEach(f => {
+            this.frames.forEach(f => {
                 ctx.save()
                 f.draw(ctx);
                 ctx.restore()
             });
         }, 
         mouseMove: function() {
-            frames.forEach(f => {
+            this.frames.forEach(f => {
                 if (f.mouseMove && f.isInteractable) f.mouseMove();
             });
         }, 
         mouseDown: function() {
-            frames.forEach(f => {
+            this.frames.forEach(f => {
                 if (f.mouseDown && f.isInteractable) f.mouseDown();
             });
         }, 
         mouseUp: function() {
-            frames.forEach(f => {
+            this.frames.forEach(f => {
                 if (f.mouseUp && f.isInteractable) f.mouseUp();
             });
         }, 
         frameEnd: function() {
-            frames.forEach(f => {
+            this.frames.forEach(f => {
                 if (f.frameEnd) f.frameEnd();
             });
         }, 
         frameStart: function() {
-            frames.forEach(f => {
+            this.frames.forEach(f => {
                 if (f.frameStart) f.frameStart();
             });
         },
