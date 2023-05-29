40c66108ab8655ec342eaf2dba14ea14abdedf1b
Frederik Haagensen <haag@itu.dk>
2023-05-29 11:06:10 +0200

    Fix naming

--- draw.js
+++ draw.js
@@ -1,8 +1,9 @@
 "use strict";
+const l = console.log;
 
 function drawDynamic(time, r) {
 
-    let canvas = document.getElementById("dynamic");
+    let canvas = document.getElementById("canvas");
     let width = canvas.width;
     let height = canvas.height;
     let ctx = canvas.getContext("2d");
@@ -30,5 +31,33 @@ function drawDynamic(time, r) {
 }
 
 function initialize() {
-    requestAnimationFrame(time => drawDynamic(time, undefined));
+    // requestAnimationFrame(time => drawDynamic(time, undefined));
+
+    const points = [];
+    const canvas = document.getElementById('canvas')
+    document.addEventListener('mousemove', e => {
+        if (e.target === canvas) {
+            if ((e.buttons & 0x1) == 0) return;
+
+            let x = e.x - canvas.offsetLeft;
+            let y = e.y - canvas.offsetTop;
+            points.push([x, y]);
+            
+
+
+        }
+    });
+    let t = time => {
+        l(points, time)
+
+        const ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for (const p of points) {
+            ctx.beginPath();
+            ctx.arc(p[0], p[1], 1, 0, 2 * Math.PI);
+            ctx.fill();
+        }
+        setTimeout(t, 1000);
+    };
+    setTimeout(t, 1000);
 }
