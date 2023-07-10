7b29fcf7f1e042e088cc91186b10343fe14837b4
Frederik Haagensen <frehaa@hotmail.dk>
2023-07-10 13:27:25 +0200

    Cleanup midi.js

--- public/scripts/midi.js
+++ public/scripts/midi.js
@@ -151,47 +151,6 @@ function initializeMIDIUSBAccess() {
     }
 }
 
-function playSound() {
-    // request microphone access
-navigator.mediaDevices.getUserMedia({ audio: true })
-  .then(() => {
-      // Create an AudioContext object
-  const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-
-  // Create an OscillatorNode
-  const oscillator = audioCtx.createOscillator();
-
-  // Set the oscillator frequency to middle C (261.63 Hz)
-  oscillator.frequency.value = 256;
-
-  // Create a GainNode to control the volume
-  const gainNode = audioCtx.createGain();
-  const duration = 2;
-
-  // Set the initial gain to 0
-  gainNode.gain.setValueAtTime(0, audioCtx.currentTime);
-
-  // Ramp up the gain to 1 over 50 milliseconds (attack)
-  gainNode.gain.linearRampToValueAtTime(1, audioCtx.currentTime + 0.05);
-
-  // Ramp down the gain to 0 over 500 milliseconds (decay)
-  gainNode.gain.linearRampToValueAtTime(0, audioCtx.currentTime + duration);
-
-  // Connect the oscillator to the gain node, and the gain node to the AudioContext destination
-  oscillator.connect(gainNode);
-  gainNode.connect(audioCtx.destination);
-
-  // Start the oscillator
-  oscillator.start();
-
-  // Stop the oscillator after 500 milliseconds
-  oscillator.stop(audioCtx.currentTime + duration);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
-}
 
 function draw(wholeNoteImage, notes, time) {
     let canvas = document.getElementById('note-canvas');
@@ -340,16 +299,9 @@ function testDeltaTime() {
 }
 
 function initialize() {
-    // playSound();
-    // defaultCode();
-
-
-    testDeltaTime();
-
     // How do we want the file thing to work? 
     // 1. Wait for user input (e.g. set up file handler)
     // 2. Given input read it, parse it and send it to somewhere else
-
 
     // const myFirstPromise = new Promise((resolve, reject) => {
     //     // We call resolve(...) when what we were doing asynchronously was successful, and reject(...) when it failed.
@@ -395,133 +347,4 @@ function initialize() {
     // // Width 40
     // // Height 25
     // // Ratio = 40/25
-
-    // const textArea = document.getElementById('code-text-area');
-    // document.onkeyup = function(event) {
-    //     if (event.ctrlKey && event.key == ENTER_KEY) {
-    //         let f = new Function('a', 'l', textArea.value);
-    //         let res = f(arrayBuffer, console.log);
-    //     }
-    // }
-
-}
-
-function defaultCode() {
-    let code = `
-l()
-let dataView = new DataView(a);
-const META_EVENT = 0xFF;
-const SYS_EX_EVENT = 0xF0;
-
-function getString(dataView, offset, length) {
-    let string = "";
-    for (let i = 0; i < length; i++) {
-        string += String.fromCharCode(dataView.getInt8(offset + i));
-    }
-    return string;
-}
-
-let header = getString(dataView, 0, 4);
-
-let length = dataView.getUint32(4);
-
-let fileFormat = dataView.getUint16(8);
-let numTracks = dataView.getUint16(10);
-let timeDivision = dataView.getUint16(12);
-
-
-let trackHeader = getString(dataView, 14, 4);
-
-
-let trackLength = dataView.getUint32(18);
-
-
-// Tries to parse a variable length value in dataView from the offset
-function getDeltaTime(dataView, offset) {
-    let delta = 0
-    let length = 0;
-    while (true) {
-        let deltaByte = dataView.getUint8(offset + length)
-        length++;
-        delta = (delta << 7) | (deltaByte & 0x7F);
-        if ((deltaByte & 0x80) === 0) break;
-    } 
-
-    return [delta, length]
-}
-
-function isNoteOnEvent(eventType) {
-    return (eventType & 0xF0) === 0x90;
-}
-function isNoteOffEvent(eventType) {
-    return (eventType & 0xF0) === 0x80;
-}
-function isControlChangeEvent(eventType) {
-    return (eventType & 0xF0) === 0xB0;
-}
-function isProgramChangeEvent(eventType) {
-    return (eventType & 0xF0) === 0xC0;
-}
-function isSystemExclusiveEvent(eventType) {
-    return (eventType & 0xFF) === 0xF0;
-}
-function isMetaEvent(eventType) {
-    return eventType === 0xFF;
-}
-
-function getMetaEventType(dataView, offset) {
-    return dataView.getUint8(offset);
-}
-
-function parseTrackEvent(dataView, offset) {
-    let eventType = dataView.getUint8(offset);
-    if (isNoteOnEvent(eventType)) {
-        l(offset, "Note On");
-        return offset + 3;
-    } else if (isNoteOffEvent(eventType)) {
-        l(offset, "Note Off");
-        return offset + 3;        
-    } else if (isControlChangeEvent(eventType)) {
-        l(offset, "Control Change");
-        return offset + 3;
-    } else if (isProgramChangeEvent(eventType)) {
-        l(offset, "Program Change");
-        return offset + 2;
-    } else if (isSystemExclusiveEvent(eventType)) {
-        l(offset, "System Exclusive");
-        return offset + 1;
-    } else if (isMetaEvent(eventType)) {
-        let metaEventType = dataView.getUint8(offset + 1)
-        let length = dataView.getUint8(offset + 2);
-        l(offset, "Meta Event", "0x"+metaEventType.toString(16), length);
-        for (let i = 0; i < length; ++i) {
-            let val = dataView.getUint8(offset + 3 + i);
-            l(val)
-        }
-        return offset + 3 + length + 1; // I do not understand why the + 1
-    } else {
-        l(offset, "Unknown Event: 0x" + eventType.toString(16));
-        return offset + 1;
-    }
-}
-
-let [vTime, vTimeLength] = getDeltaTime(dataView, 22)
-l(trackLength)
-
-
-return 
-let offset = 23 + trackLength;
-let i = 0;
-while(i < 5) {
-    i++;
-    offset = parseTrackEvent(dataView, offset);
-}
-
-// Start of next track
-l(getString(dataView, 22 + trackLength, 4));
-
-`;
-
-    document.getElementById('code-text-area').innerHTML = code;
-
 }
